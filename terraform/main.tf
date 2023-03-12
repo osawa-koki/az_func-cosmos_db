ae9c1e9aca79a50efd7d48f3bcd1e65197b6f4e3
osawa-koki <github@osawa.dev>
2023-03-12 16:25:10 +0900

    backend "azurerm"

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,19 @@
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "general"
+    storage_account_name = "terraform4osawakoki"
+    container_name       = "azfunc-cosmosdb"
+    key                  = "azfunc-cosmosdb.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
