b817702c6af0e3b6cf920cd9e06ed0295411f4d6
osawa-koki <github@osawa.dev>
2023-03-12 16:39:01 +0900

    小さな修正。

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -12,7 +12,7 @@ resource "azurerm_app_service_plan" "service_plan" {
 
 resource "azurerm_function_app" "az_func" {
   name                      = var.function_app_name
-  location                  = var.location
+  location                  = azurerm_resource_group.resource_group.location
   resource_group_name       = azurerm_resource_group.resource_group.name
   app_service_plan_id       = azurerm_app_service_plan.service_plan.id
   storage_connection_string = azurerm_storage_account.storage_account.primary_blob_connection_string
