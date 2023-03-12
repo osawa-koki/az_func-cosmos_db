5b4376fb00a63f476ba3f7bf7562d4b1e8e6e72c
osawa-koki <github@osawa.dev>
2023-03-12 17:23:13 +0900

    非推奨モジュール直した。

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,22 +1,19 @@
 
-resource "azurerm_app_service_plan" "service_plan" {
+resource "azurerm_service_plan" "service_plan" {
   name                = var.project_name
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  os_type             = "Linux"
+  sku_name            = "Y1"
 }
 
-resource "azurerm_function_app" "az_func" {
+resource "azurerm_linux_function_app" "az_func" {
   name                       = var.function_app_name
   location                   = azurerm_resource_group.resource_group.location
   resource_group_name        = azurerm_resource_group.resource_group.name
-  app_service_plan_id        = azurerm_app_service_plan.service_plan.id
+  service_plan_id            = azurerm_service_plan.service_plan.id
   storage_account_name       = azurerm_storage_account.storage_account.name
   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
-  version                    = "~4"
   https_only                 = true
+  site_config {}
 }
