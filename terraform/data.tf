30143e6cf163e51202f0a8b6bc2f39805f97930f
osawa-koki <github@osawa.dev>
2023-03-12 16:36:49 +0900

    リファクタリング。

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,16 +1,16 @@
 
 // https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/function_app
 resource "azurerm_storage_account" "storage_account" {
-  name = replace(var.storage_account_name, "-", "")
-  resource_group_name = azurerm_resource_group.resource_group.name
-  location = azurerm_resource_group.resource_group.location
-  account_tier = "Standard"
+  name                     = replace(var.storage_account_name, "-", "")
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_app_service_plan" "service_plan" {
-  name = var.project_name
-  location = azurerm_resource_group.resource_group.location
+  name                = var.project_name
+  location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
 
   sku {
