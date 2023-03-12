d721bf884ce2e38a6979625d973e6e6a4e6b183d
osawa-koki <github@osawa.dev>
2023-03-12 16:36:37 +0900

    リファクタリング。

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -5,6 +5,6 @@ resource "azurerm_function_app" "az_func" {
   resource_group_name       = azurerm_resource_group.resource_group.name
   app_service_plan_id       = azurerm_app_service_plan.app_service_plan.id
   storage_connection_string = azurerm_storage_account.storage_account.primary_blob_connection_string
-  version = "~4"
-  https_only = true
+  version                   = "~4"
+  https_only                = true
 }
