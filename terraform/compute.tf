bfee0e445899f792ef43d27f3e39a477dc7eec3a
osawa-koki <github@osawa.dev>
2023-03-12 17:19:21 +0900

    hashicorp/azurerm -> 3.0.0

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -11,11 +11,12 @@ resource "azurerm_app_service_plan" "service_plan" {
 }
 
 resource "azurerm_function_app" "az_func" {
-  name                      = var.function_app_name
-  location                  = azurerm_resource_group.resource_group.location
-  resource_group_name       = azurerm_resource_group.resource_group.name
-  app_service_plan_id       = azurerm_app_service_plan.service_plan.id
-  storage_connection_string = azurerm_storage_account.storage_account.primary_blob_connection_string
-  version                   = "~4"
-  https_only                = true
+  name                       = var.function_app_name
+  location                   = azurerm_resource_group.resource_group.location
+  resource_group_name        = azurerm_resource_group.resource_group.name
+  app_service_plan_id        = azurerm_app_service_plan.service_plan.id
+  storage_account_name       = azurerm_storage_account.storage_account.name
+  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+  version                    = "~4"
+  https_only                 = true
 }
