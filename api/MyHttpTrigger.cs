781dcb7e4c252e24d7e49ceee27ce305b209d4b7
osawa-koki <github@osawa.dev>
2023-03-13 22:30:33 +0900

    小さな修正。

--- api/MyHttpTrigger.cs
+++ api/MyHttpTrigger.cs
@@ -21,7 +21,7 @@ namespace azfunc_cosmosdb
     [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
     public async Task<IActionResult> Run(
-      [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+      [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
     {
       _logger.LogInformation("C# HTTP trigger function processed a request.");
 
