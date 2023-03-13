a1e24e006a0efefc0a4b8414f186bf942ce8f3b6
osawa-koki <github@osawa.dev>
2023-03-13 17:31:03 +0900

    Select

---
+++ api/Select.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.OpenApi.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+
+namespace azfunc_cosmosdb
+{
+  public partial class Program
+  {
+    [FunctionName("Select")]
+    [OpenApiOperation(operationId: "Run", tags: new[] { "user" })]
+    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+    [OpenApiParameter(name: "id", In = ParameterLocation.Path, Required = false, Type = typeof(string), Description = "The **id** parameter")]
+    [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "The **name** parameter")]
+    [OpenApiParameter(name: "profession", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "The **profession** parameter")]
+    [OpenApiParameter(name: "age", In = ParameterLocation.Query, Required = false, Type = typeof(int), Description = "The **age** parameter")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+    public async Task<IActionResult> Select(
+      [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req)
+    {
+      string id = req.Query["id"];
+      string name = req.Query["name"];
+      string profession = req.Query["profession"];
+      string age = req.Query["age"];
+
+      var filterBuilder = Builders<BsonDocument>.Filter;
+      var filter = filterBuilder.Empty;
+
+      if (id != null)
+      {
+        filter = filter & filterBuilder.Eq("_id", new ObjectId(id));
+      }
+      if (name != null)
+      {
+        filter = filter & filterBuilder.Eq("name", name);
+      }
+      if (profession != null)
+      {
+        filter = filter & filterBuilder.Eq("profession", profession);
+      }
+      if (age != null)
+      {
+        filter = filter & filterBuilder.Eq("age", int.Parse(age));
+      }
+
+      var documents = await users_collection.Find(filter).ToListAsync();
+
+      return new OkObjectResult(documents);
+    }
+  }
+}
